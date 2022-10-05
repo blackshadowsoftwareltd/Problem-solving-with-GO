0312e9bad3277bfbdc1983d9b43be0a99dd87203
remon <blackshadowsoftwareltd@gmail.com>
2022-10-06 00:27:53 +0600

    done

--- 19.Count-Odd-Numbers-below-n/main.go
+++ 19.Count-Odd-Numbers-below-n/main.go
@@ -5,26 +5,9 @@ import (
 )
 
 func main() {
-
-	fmt.Println(OddCount(7))
+	fmt.Println(OddCount(15))
 }
 
 func OddCount(n int) int {
-	var arr []int
-
-	for i := n - 1; i > 0; i-- {
-		if i%2 != 0 {
-			arr = append(arr, i)
-		}
-	}
-	fmt.Println(arr)
-	for len(arr) != 1 {
-		arr = arr[:1]
-		fmt.Println(arr)
-		if len(arr) != 1 {
-			arr = arr[len(arr)-1:]
-			fmt.Println(arr)
-		}
-	}
-	return arr[0]
+	return n / 2
 }
